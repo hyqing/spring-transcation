490cdfdfec80d2e49122a2bfc3e967868861f33f
hyq07508 <hyq07508@ly.com>
2020-12-15 17:36:29 +0800

    添加注释

--- src/main/java/org/example/spring/transcation/Application.java
+++ src/main/java/org/example/spring/transcation/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /**
  * Created by hyq on 2020/12/14 17:22.
  */
+//exclude = {DataSourceAutoConfiguration.class}，用于解决循环依赖
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
