59a045b7d4e907ace19931546479cc53626fb328
hyq07508 <hyq07508@ly.com>
2020-12-15 17:13:13 +0800

    ThreadLocal 用于提供线程局部变量

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 1.JPA 多数据源
 2.JPA 事务
 3.JPA，读写分离.使用
+
+参考资料：
+https://blog.csdn.net/mythread/article/details/107468587?utm_medium=distribute.pc_relevant_download.none-task-blog-baidujs-1.nonecase&depth_1-utm_source=distribute.pc_relevant_download.none-task-blog-baidujs-1.nonecase
--- src/main/java/org/example/spring/transcation/config/DataSourceRouter.java
+++ src/main/java/org/example/spring/transcation/config/DataSourceRouter.java
@@ -7,15 +7,25 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
  */
 public class DataSourceRouter extends AbstractRoutingDataSource {
 
-    private static String dataSourceConfig = "master"; //默认主库
+    /**
+     * ThreadLocal 用于提供线程局部变量，在多线程环境可以保证各个线程里的变量独立于其它线程里的变量。
+     * 也就是说 ThreadLocal 可以为每个线程创建一个【单独的变量副本】，相当于线程的 private static 类型变量。
+     */
+    private static final ThreadLocal<String> CONTEXT_HOLDER = new ThreadLocal<>();
+
+//    private static String dataSourceConfig = "master"; //默认主库
 
     @Override
     protected Object determineCurrentLookupKey() {
-        System.out.println("当前数据源:" + dataSourceConfig);
-        return dataSourceConfig;
+        System.out.println("当前数据源:" + getDataSource());
+        return getDataSource();
     }
 
     public static void setMyDataSource(String dataSource) {
-        dataSourceConfig = dataSource;
+        CONTEXT_HOLDER.set(dataSource);
+    }
+
+    public static String getDataSource() {
+        return CONTEXT_HOLDER.get();
     }
 }
